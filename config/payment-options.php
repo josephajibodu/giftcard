7542e2416f931c491a56af8ebbcaf638b647874f
cremirdevio <josephajibodu@gmail.com>
2024-07-18 23:34:53 +0100

    purchase working fine

---
+++ config/payment-options.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+    [
+        "name" => "Amazon",
+        "label" => "E-Card",
+        "image" => "amazon.webp",
+        "slug" => "amazon"
+    ],
+    [
+        "name" => "Ebay",
+        "label" => "E-Card",
+        "image" => "ebay.png",
+        "slug" => "ebay"
+    ],
+    [
+        "name" => "Steam",
+        "label" => "E-Card",
+        "image" => "steam.webp",
+        "slug" => "steam"
+    ],
+    [
+        "name" => "Apple",
+        "label" => "E-Card",
+        "image" => "itunes.webp",
+        "slug" => "apple"
+    ],
+    [
+        "name" => "American Express",
+        "label" => "E-Card",
+        "image" => "american-express.webp",
+        "slug" => "american-express"
+    ],
+    [
+        "name" => "Google Play",
+        "label" => "E-Card",
+        "image" => "google-play.png",
+        "slug" => "google-play"
+    ],
+    [
+        "name" => "Macy's",
+        "label" => "E-Card",
+        "image" => "macys.png",
+        "slug" => "macys"
+    ],
+    [
+        "name" => "Razer Gold",
+        "label" => "E-Card",
+        "image" => "razer.webp",
+        "slug" => "razer-gold"
+    ],
+    [
+        "name" => "US Spotify",
+        "label" => "E-Card",
+        "image" => "spotify.webp",
+        "slug" => "us-spotify"
+    ],
+    [
+        "name" => "US Hulu",
+        "label" => "E-Card",
+        "image" => "hulu.webp",
+        "slug" => "us-hulu"
+    ],
+    [
+        "name" => "US PSN",
+        "label" => "E-Card",
+        "image" => "play-station.webp",
+        "slug" => "us-psn"
+    ],
+    [
+        "name" => "US Paramount+",
+        "label" => "E-Card",
+        "image" => "paramount.webp",
+        "slug" => "us-paramount-plus"
+    ],
+    [
+        "name" => "Visa Gift",
+        "label" => "E-Card",
+        "image" => "visa.webp",
+        "slug" => "visa-gift"
+    ],
+    [
+        "name" => "Visa Vanilla",
+        "label" => "E-Card",
+        "image" => "vanilla.webp",
+        "slug" => "visa-vanilla"
+    ],
+    [
+        "name" => "Sephora",
+        "label" => "E-Card",
+        "image" => "sephora.png",
+        "slug" => "sephora"
+    ],
+    [
+        "name" => "Nordstrom",
+        "label" => "E-Card",
+        "image" => "nordstorm.png",
+        "slug" => "nordstrom"
+    ]
+];
