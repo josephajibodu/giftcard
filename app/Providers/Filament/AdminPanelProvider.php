a05ce441389a2da24427282bb82e74f8dacbdf56
cremirdevio <josephajibodu@gmail.com>
2024-09-13 19:47:20 +0100

    enable registration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->profile()
             ->colors([
                 'primary' => Color::Purple,
