02e7c72535bcd2f13d9a45af9ce7b97ee2d47eb5
Joseph Ajibodu <josephajibodu@gmail.com>
2026-07-15 17:39:08 +0100

    done

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,7 @@ class User extends Authenticatable
 
     public function canAccessPanel(Panel $panel): bool
     {
+        return true;
         return str_ends_with($this->email, 'egiftchecker.com'); //$this->hasVerifiedEmail();
     }
 }
